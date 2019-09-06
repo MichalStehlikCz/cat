6d6851926522a281fff9bbdfc6e296aceecdb869
Michal Stehlik <stehlik@provys.com>
2019-09-06 19:53:56 +0200

    Testing generated domain classes

--- dbloader/src/main/java/com/provys/catalogue/dbloader/DomainDbLoadRunner.java
+++ dbloader/src/main/java/com/provys/catalogue/dbloader/DomainDbLoadRunner.java
@@ -1,33 +1,33 @@
 package com.provys.catalogue.dbloader;
 
 import com.provys.catalogue.api.Domain;
-import com.provys.catalogue.dbloader.db.tables.records.KerDomainTbRecord;
 import com.provys.catalogue.impl.DomainManagerImpl;
 import com.provys.catalogue.impl.DomainProxy;
 import com.provys.catalogue.impl.gen.DomainValue;
 import com.provys.provysdb.ProvysDbContext;
 import com.provys.provysobject.impl.ProvysObjectLoadRunner;
-import org.jooq.Condition;
-import org.jooq.Field;
-import org.jooq.impl.DSL;
-
-import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
+import java.lang.Boolean;
+import java.lang.Integer;
+import java.lang.Override;
+import java.lang.String;
 import java.math.BigInteger;
 import java.util.List;
 import java.util.Objects;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import org.jooq.Condition;
+import org.jooq.Record13;
+import org.jooq.impl.DSL;
 
-import static com.provys.catalogue.dbloader.db.Tables.KER_DOMAIN_TB;
-
-class DomainDbLoadRunner
-        extends ProvysObjectLoadRunner<Domain, DomainValue, DomainProxy, DomainManagerImpl, KerDomainTbRecord> {
-
+class DomainDbLoadRunner extends ProvysObjectLoadRunner<Domain, DomainValue, DomainProxy, DomainManagerImpl, Record13<BigInteger, String, String, String, Integer, Integer, String, Boolean, Boolean, Boolean, String, String, Boolean>> {
     @Nonnull
     private final ProvysDbContext dbContext;
+
     @Nullable
     private final Condition condition;
 
-    DomainDbLoadRunner(DomainManagerImpl manager, ProvysDbContext dbContext, @Nullable Condition condition) {
+    DomainDbLoadRunner(DomainManagerImpl manager, ProvysDbContext dbContext,
+                       @Nullable Condition condition) {
         super(manager);
         this.dbContext = Objects.requireNonNull(dbContext);
         this.condition = condition;
@@ -35,30 +35,29 @@ class DomainDbLoadRunner
 
     @Nonnull
     @Override
-    protected List<KerDomainTbRecord> select() {
-        List<KerDomainTbRecord> result;
+    protected List<Record13<BigInteger, String, String, String, Integer, Integer, String, Boolean, Boolean, Boolean, String, String, Boolean>> select(
+    ) {
+        List<Record13<BigInteger, String, String, String, Integer, Integer, String, Boolean, Boolean, Boolean, String, String, Boolean>> result;
         try (var dsl = dbContext.createDSL()) {
-            result = dsl.select(KER_DOMAIN_TB.DOMAIN_ID)
-                    .from(KER_DOMAIN_TB)
-                    .where(condition == null ? DSL.noCondition() : condition).
-                    fetch().into(KerDomainTbRecord.class);
+            result = dsl.select(DSL.field("DOMAIN_ID", BigInteger.class), DSL.field("NAME_NM", String.class), DSL.field("NAME", String.class), DSL.field("DATATYPE_NM", String.class), DSL.field("DATALENGTH", Integer.class), DSL.field("DATAPRECISION", Integer.class), DSL.field("NOTE", String.class), DSL.field("ALLOWED", Boolean.class), DSL.field("QVISIBLE", Boolean.class), DSL.field("LVISIBLE", Boolean.class), DSL.field("VALIDATE_CD", String.class), DSL.field("LFORMAT_CD", String.class), DSL.field("NATORDER", Boolean.class))
+                    .from(DSL.table("KER_DOMAIN_TB"))
+                    .where(condition == null ? DSL.noCondition() : condition)
+                    .fetch();
         }
         return result;
     }
 
     @Nonnull
     @Override
-    protected BigInteger getId(KerDomainTbRecord sourceObject) {
-        return sourceObject.getDomainId();
+    protected BigInteger getId(
+            Record13<BigInteger, String, String, String, Integer, Integer, String, Boolean, Boolean, Boolean, String, String, Boolean> sourceObject) {
+        return sourceObject.get("DOMAIN_ID", BigInteger.class);
     }
 
     @Nonnull
     @Override
-    protected DomainValue createValueObject(KerDomainTbRecord sourceObject) {
-        return new DomainValue(sourceObject.getDomainId(), sourceObject.getNameNm(), sourceObject.getName(),
-                sourceObject.getDatatypeNm(), sourceObject.getDatalength(), sourceObject.getDataprecision(),
-                sourceObject.getQvisible(), sourceObject.getLvisible(), sourceObject.getValidateCd(),
-                sourceObject.getLformatCd(), sourceObject.getNatorder(), sourceObject.getNote(),
-                sourceObject.getAllowed());
+    protected DomainValue createValueObject(
+            Record13<BigInteger, String, String, String, Integer, Integer, String, Boolean, Boolean, Boolean, String, String, Boolean> sourceObject) {
+        return new DomainValue(getId(sourceObject), sourceObject.get("NAME_NM", String.class), sourceObject.get("NAME", String.class), sourceObject.get("DATATYPE_NM", String.class), sourceObject.get("DATALENGTH", Integer.class), sourceObject.get("DATAPRECISION", Integer.class), sourceObject.get("NOTE", String.class), sourceObject.get("ALLOWED", Boolean.class), sourceObject.get("QVISIBLE", Boolean.class), sourceObject.get("LVISIBLE", Boolean.class), sourceObject.get("VALIDATE_CD", String.class), sourceObject.get("LFORMAT_CD", String.class), sourceObject.get("NATORDER", Boolean.class));
     }
 }
--- dbloader/src/main/java/com/provys/catalogue/dbloader/DomainDbLoader.java
+++ dbloader/src/main/java/com/provys/catalogue/dbloader/DomainDbLoader.java
@@ -1,21 +1,22 @@
 package com.provys.catalogue.dbloader;
 
-import com.provys.catalogue.dbloader.db.tables.records.KerDomainTbRecord;
 import com.provys.catalogue.impl.DomainLoaderBase;
 import com.provys.catalogue.impl.DomainManagerImpl;
 import com.provys.provysdb.ProvysDbContext;
-
+import java.lang.Boolean;
+import java.lang.Integer;
+import java.lang.Override;
+import java.lang.String;
+import java.math.BigInteger;
+import java.util.Objects;
 import javax.annotation.Nonnull;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
-import java.math.BigInteger;
-import java.util.Objects;
-
-import static com.provys.catalogue.dbloader.db.Tables.KER_DOMAIN_TB;
+import org.jooq.Record13;
+import org.jooq.impl.DSL;
 
 @ApplicationScoped
-public class DomainDbLoader extends DomainLoaderBase<KerDomainTbRecord> {
-
+public class DomainDbLoader extends DomainLoaderBase<Record13<BigInteger, String, String, String, Integer, Integer, String, Boolean, Boolean, Boolean, String, String, Boolean>> {
     @Nonnull
     private final ProvysDbContext dbContext;
 
@@ -26,20 +27,19 @@ public class DomainDbLoader extends DomainLoaderBase<KerDomainTbRecord> {
 
     @Nonnull
     @Override
-    protected DomainDbLoadRunner getLoadRunnerByNameNm(DomainManagerImpl domainManager, String nameNm) {
-        return new DomainDbLoadRunner(domainManager, dbContext, KER_DOMAIN_TB.NAME_NM.eq(nameNm));
+    protected DomainDbLoadRunner getLoadRunnerByNameNm(DomainManagerImpl manager, String nameNm) {
+        return new DomainDbLoadRunner(manager, dbContext, DSL.field("NAME_NM", String.class).eq(nameNm));
     }
 
     @Nonnull
     @Override
-    protected DomainDbLoadRunner getLoadRunnerById(DomainManagerImpl domainManager, BigInteger id) {
-        return new DomainDbLoadRunner(domainManager, dbContext, KER_DOMAIN_TB.DOMAIN_ID.eq(id));
+    protected DomainDbLoadRunner getLoadRunnerById(DomainManagerImpl manager, BigInteger id) {
+        return new DomainDbLoadRunner(manager, dbContext, DSL.field("DOMAIN_ID", BigInteger.class).eq(id));
     }
 
     @Nonnull
     @Override
-    protected DomainDbLoadRunner getLoadRunnerAll(DomainManagerImpl domainManager) {
-        return new DomainDbLoadRunner(domainManager, dbContext, null);
+    protected DomainDbLoadRunner getLoadRunnerAll(DomainManagerImpl manager) {
+        return new DomainDbLoadRunner(manager, dbContext, null);
     }
-
 }
